bd871a634b4f86dbaa93aeaf3bf35649634beb63
QuickSilver <avataraang399@gmail.com>
2019-03-27 19:23:13 +1000

    Add root route redirect

--- app/src/main/java/com/skillmasters/server/http/controller/HomeController.java
+++ app/src/main/java/com/skillmasters/server/http/controller/HomeController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController
 {
+  @RequestMapping("/")
+  public String swagger()
+  {
+    return "redirect:/swagger-ui.html";
+  }
+
   @RequestMapping("/docs")
   public String swagger()
   {
