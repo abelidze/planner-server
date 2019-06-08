4e061042f89afddb3ce666a54a8d866530137c55
QuickSilver <avataraang399@gmail.com>
2019-06-08 21:14:23 +1000

    Add dummy EventService for future use

---
+++ app/src/main/java/com/skillmasters/server/service/EventService.java
@@ -0,0 +1,12 @@
+package com.skillmasters.server.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skillmasters.server.repository.EventRepository;
+import com.skillmasters.server.model.Event;
+
+public class EventService
+{
+}
