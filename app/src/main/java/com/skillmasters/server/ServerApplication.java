4a5b18c03a7784d08d644576d53e60143178ea37
QuickSilver <avataraang399@gmail.com>
2019-06-10 03:09:56 +1000

    Try to fix strange spring-securiy bug after live reloading

--- app/src/main/java/com/skillmasters/server/ServerApplication.java
+++ app/src/main/java/com/skillmasters/server/ServerApplication.java
@@ -6,11 +6,13 @@ import biweekly.io.scribe.property.RecurrenceRuleScribe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = { "com.skillmasters.server" } )
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class })
+@ComponentScan(basePackages = { "com.skillmasters.server" })
 public class ServerApplication
 {
   public static void main(String[] args)
