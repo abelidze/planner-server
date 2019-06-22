244c888a63a55be0f9a04a0427acabfdbc5f0faf
QuickSilver <avataraang399@gmail.com>
2019-06-22 22:41:29 +1000

    Remove unused stuff from repos

--- app/src/main/java/com/skillmasters/server/repository/TaskRepository.java
+++ app/src/main/java/com/skillmasters/server/repository/TaskRepository.java
@@ -1,9 +1,5 @@
 package com.skillmasters.server.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
