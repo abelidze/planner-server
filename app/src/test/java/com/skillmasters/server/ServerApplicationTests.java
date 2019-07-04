524631646f012b84e2f0e9141939ba3b5f6d4a13
QuickSilver <avataraang399@gmail.com>
2019-07-04 20:42:41 +1000

    Add travis

--- app/src/test/java/com/skillmasters/server/ServerApplicationTests.java
+++ app/src/test/java/com/skillmasters/server/ServerApplicationTests.java
@@ -1,16 +1,16 @@
-// package com.skillmasters.server;
+package com.skillmasters.server;
 
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-// @RunWith(SpringRunner.class)
-// @SpringBootTest
-// public class ServerApplicationTests
-// {
-// 	@Test
-// 	public void contextLoads()
-// 	{
-// 	}
-// }
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerApplicationTests
+{
+	@Test
+	public void contextLoads()
+	{
+	}
+}
