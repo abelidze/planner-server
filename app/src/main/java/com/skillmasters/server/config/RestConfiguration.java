3176750105a45b57c21b75466be5a448d6eef5fe
QuickSilver <avataraang399@gmail.com>
2019-07-14 15:00:06 +1000

    Disable Data REST, enable Actuator

--- app/src/main/java/com/skillmasters/server/config/RestConfiguration.java
+++ app/src/main/java/com/skillmasters/server/config/RestConfiguration.java
@@ -1,25 +1,25 @@
-package com.skillmasters.server;
+// package com.skillmasters.server;
 
-import javax.persistence.EntityManager;
-import javax.persistence.metamodel.Type;
+// import javax.persistence.EntityManager;
+// import javax.persistence.metamodel.Type;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+// import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-@Configuration
-public class RestConfiguration implements RepositoryRestConfigurer
-{
-  @Autowired
-  private EntityManager entityManager;
+// @Configuration
+// public class RestConfiguration implements RepositoryRestConfigurer
+// {
+//   @Autowired
+//   private EntityManager entityManager;
 
-  @Override
-  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config)
-  {
-    config.exposeIdsFor(
-        entityManager.getMetamodel().getEntities().stream()
-        .map(Type::getJavaType)
-        .toArray(Class[]::new));
-  }
-}
+//   @Override
+//   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config)
+//   {
+//     config.exposeIdsFor(
+//         entityManager.getMetamodel().getEntities().stream()
+//         .map(Type::getJavaType)
+//         .toArray(Class[]::new));
+//   }
+// }
--- app/src/main/java/com/skillmasters/server/misc/EventEventHandler.java
+++ app/src/main/java/com/skillmasters/server/misc/EventEventHandler.java
@@ -1,24 +1,24 @@
-package com.skillmasters.server.misc;
+// package com.skillmasters.server.misc;
 
-import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
-import org.springframework.stereotype.Component;
-import org.springframework.data.rest.core.annotation.*;
-import com.skillmasters.server.model.Event;
+// import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
+// import org.springframework.stereotype.Component;
+// import org.springframework.data.rest.core.annotation.*;
+// import com.skillmasters.server.model.Event;
 
-public class EventEventHandler extends AbstractRepositoryEventListener
-{
-  @Override
-  public void onBeforeSave(Object entity)
-  {
-    System.out.println("SAVE");
-  }
+// public class EventEventHandler extends AbstractRepositoryEventListener
+// {
+//   @Override
+//   public void onBeforeSave(Object entity)
+//   {
+//     System.out.println("SAVE");
+//   }
 
-  @Override
-  public void onAfterDelete(Object entity)
-  {
-    System.out.println("DELETE");
-  }
-}
+//   @Override
+//   public void onAfterDelete(Object entity)
+//   {
+//     System.out.println("DELETE");
+//   }
+// }
 
 // @Component
 // @RepositoryEventHandler
