2da538478b28afca73b4a6d5b0f91918837ea823
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-07-15 15:41:07 +1000

    event pattern tests

---
+++ app/src/test/java/com/skillmasters/server/model/EventPatternTests.java
@@ -0,0 +1,61 @@
+package com.skillmasters.server.model;
+import com.skillmasters.server.common.EventGenerator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventPatternTests extends ModelTests
+{
+  @Test
+  public void testGettersSetters()
+  {
+    EventPattern ep = new EventPattern();
+
+    ep.setId(testLong);
+    assertThat(ep.getId()).isEqualTo(testLong);
+
+    Event e = EventGenerator.genEvent(100);
+    ep.setEvent(e);
+    assertThat(ep.getEvent()).isEqualTo(e);
+
+    ep.setDuration(testLong);
+    assertThat(ep.getDuration()).isEqualTo(testLong);
+
+    ep.setTimezone(testString);
+    assertThat(ep.getTimezone()).isEqualTo(testString);
+
+    ep.setRrule(testString);
+    assertThat(ep.getRrule()).isEqualTo(testString);
+
+    //todo: exrules
+
+    ep.setStartedAt(testDateStart);
+    assertThat(ep.getStartedAt()).isEqualTo(testDateStart);
+
+    ep.setEndedAt(testDateEnd);
+    assertThat(ep.getEndedAt()).isEqualTo(testDateEnd);
+
+    ep.setUpdatedAt(testDateStart);
+    assertThat(ep.getUpdatedAt()).isEqualTo(testDateStart);
+
+    ep.setCreatedAt(testDateStart);
+    assertThat(ep.getCreatedAt()).isEqualTo(testDateStart);
+  }
+
+  @Test
+  public void testGetEventId()
+  {
+    Event e = EventGenerator.genEventWithOwner(100, "127");
+    e.setId(113L);
+    EventPattern ep = new EventPattern();
+    ep.setEvent(e);
+
+    assertThat(ep.getEventId()).isEqualTo(113L);
+  }
+
+}
